1359ff964d89eab0c3dc072740c4c0012d1e7a7a
vanya <vanyakalmykov2022@mail.ru>
2024-07-06 15:32:57 +0300

    Изменения Component

--- KTSF/Components/TabComponents/StaffComponent/StaffComponent.cs
+++ KTSF/Components/TabComponents/StaffComponent/StaffComponent.cs
@@ -1,6 +1,8 @@
 ﻿using KTSF.ViewModel;
+using KTSFClassLibrary;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,24 +13,34 @@ namespace KTSF.Components.TabComponents.StaffComponent
 {
     public class StaffComponent : TabComponent
     {
+        public ObservableCollection<User> Users { get; } = new ObservableCollection<User>();
+
         public StaffComponent(UserControlVM binding, AppControl appControl) : base(binding, appControl)
         {
         }
 
         public override UserControl Initial() => new StaffUC(this);
 
-        public override async void UserControl_Loaded(object sender, RoutedEventArgs e)
+        public async override void ComponentLoaded()
         {
-
             IsLoad = "Загрузка пользователей";
 
-            await Task.Delay(2000);
+            await Load();
 
             IsLoad = null;
+        }
 
+        public async Task Load()
+        {
+            List<User> users = await AppControl.Server.GetUsers(); 
+
+            foreach (User user in users) {
+                Users.Add(user);
+            }
+             
         }
 
 
-       
+
     }
 }
