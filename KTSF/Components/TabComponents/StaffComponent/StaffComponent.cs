6ff095236371bb515b385e9451043aef1a7f0e41
vanya <vanyakalmykov2022@mail.ru>
2024-07-06 14:28:40 +0300

    add function LoadedComponet

--- KTSF/Components/TabComponents/StaffComponent/StaffComponent.cs
+++ KTSF/Components/TabComponents/StaffComponent/StaffComponent.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace KTSF.Components.TabComponents.StaffComponent
@@ -15,5 +16,19 @@ namespace KTSF.Components.TabComponents.StaffComponent
         }
 
         public override UserControl Initial() => new StaffUC(this);
+
+        public override async void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            IsLoad = "Загрузка пользователей";
+
+            await Task.Delay(2000);
+
+            IsLoad = null;
+
+        }
+
+
+       
     }
 }
